446026420da0c0998ad1aa54119dbe114798f1c9
Soufiane-Chajjaoui <schajjaoui@gmail.com>
2024-02-22 20:14:37 +0100

    Repositories interfaces of our Entities

--- src/main/java/est/projet/quizapp/repositories/RepoQuestion.java
+++ src/main/java/est/projet/quizapp/repositories/RepoQuestion.java
@@ -1,9 +1,13 @@
 package est.projet.quizapp.repositories;
 
 import est.projet.quizapp.entities.Question;
+import est.projet.quizapp.entities.Reponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RepoQuestion extends JpaRepository<Question , Long> {
+    List<Question> findByQuizId(Long id);
 }
--- src/main/java/est/projet/quizapp/repositories/RepoReponse.java
+++ src/main/java/est/projet/quizapp/repositories/RepoReponse.java
@@ -4,6 +4,10 @@ import est.projet.quizapp.entities.Reponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RepoReponse extends JpaRepository<Reponse , Long> {
+    Optional<List<Reponse>> findByQuestionId(Long id);
 }
